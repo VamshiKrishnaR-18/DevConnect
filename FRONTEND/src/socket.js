b8abb4a17200fff595d4afa6cd2896a0ad89f567
VamshiKrishna <vamshikrishna7923@gmail.com>
2025-07-17 22:52:51 +0530

    Update socket.js

--- FRONTEND/src/socket.js
+++ FRONTEND/src/socket.js
@@ -1,4 +1,7 @@
 import {io} from "socket.io-client";
-const socket = io("http://localhost:3000");
+const socket = io("https://devconnect-f4au.onrender.com", {
+  withCredentials: true,
+  transports: ["websocket", "polling"]
+});
 
 export default socket;
