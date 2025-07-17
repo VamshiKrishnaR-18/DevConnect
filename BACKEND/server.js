979efc83f4cba9a46d8e1d9ca4e0990d44a24313
VamshiKrishna <vamshikrishna7923@gmail.com>
2025-07-17 23:17:17 +0530

    Update server.js

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -20,8 +20,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: [
-      "http://localhost:5173",
-      "http://localhost:5174",
+      "https://dev-connect-beryl.vercel.app",
       process.env.FRONTEND_URL
     ].filter(Boolean),
     credentials: true,
